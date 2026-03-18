9e91d8865545fcf0e24d6ab37435fcfb0ab4317f
Kyoto-01 <mendesjonatas007@gmail.com>
2026-03-17 23:28:35 -0300

    ADD: autorization token URI.

--- core/views.py
+++ core/views.py
@@ -83,5 +83,5 @@ def caramelosec_token_view(request):
         return HttpResponseNotAllowed(['GET'])
 
     return JsonResponse({
-        'caramelosec-token': 'b1d15290-1f1a-4051-b58a-470999accebe',
+        'caramelosec-token': '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
     })

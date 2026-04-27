3193934614ffb92e908db0c24feb09db8c4cf76d
Kyoto-01 <mendesjonatas007@gmail.com>
2026-04-26 22:26:02 -0300

    feat: liberando /admin com senha fraca.

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,20 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        from django.db.models.signals import post_migrate
+        from django.dispatch import receiver
+        
+        @receiver(post_migrate)
+        def create_superuser(sender, **kwargs):
+            from django.contrib.auth.models import User
+            
+            # Só cria se não existir
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    email='admin@example.com',
+                    password='12345678'
+                )
+                print("✓ Admin criado: admin / 12345678")
